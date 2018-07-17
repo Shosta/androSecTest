2b0deea755b44b6c898373c5a3e9bdf028e097bf
Unknown <remi.lavedrine@outlook.com>
2018-07-17 16:54:07 +0200

    Add the base app and handle the terminal command args
    The terminal command args are handled with "github.com/alexflint.go-args"
    Adding the variables

---
+++ androSecTest.go
@@ -0,0 +1,40 @@
+/*
+Copyright 2018 Rémi Lavedrine.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package main
+
+import (
+	"github.com/alexflint/go-arg"
+	"github.com/shosta/androSecTest/variables"
+)
+
+var args struct {
+	AppName string `arg:"-a" help:"application name"`
+	Dest    string `arg:"-d" help:"destination folder absolute path"`
+	Verbose bool   `arg:"-v" help:"verbosity level"`
+}
+
+func main() {
+	arg.MustParse(&args)
+
+	if args.AppName == "" {
+		// Wait for the user input to get the package.
+	}
+
+	if args.Verbose {
+		variables.IsVerboseLogRequired = true
+	}
+
+}
---
+++ variables/variables.go
@@ -0,0 +1,3 @@
+package variables
+
+var IsVerboseLogRequired bool = false
