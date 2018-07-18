0a7a7f9abc5e62928519e0d6ffa69fca80290f34
Unknown <remi.lavedrine@outlook.com>
2018-07-18 16:49:43 +0200

    Add a "RunAlias" command to be able to run a user defined alias

--- command/command.go
+++ command/command.go
@@ -29,31 +29,27 @@ func RunCmd(cmd string) string {
 	return string(out)
 }
 
-// func RunAlias(cmd string, args []string) string {
-// 	binary, lookErr := exec.LookPath("/bin/bash")
-// 	if lookErr != nil {
-// 		logging.PrintlnDebug("look")
-// 		panic(lookErr)
-// 	}
-
-// 	args2 := []string{"-c", "signapk", cmd}
-
-// 	env := os.Environ()
-
-// 	execErr := syscall.Exec(binary, args2, env)
-// 	if execErr != nil {
-// 		logging.PrintlnDebug("sys")
-// 		panic(execErr)
-// 	}
-
-// 	return "done"
-
-// 	testCmd := exec.Command("java", "-jar", "/home/shosta/ShostaSyncBox/Developpement/HackingTools/SignApkUtils/sign.jar", cmd+".apk")
-// 	testOut, err := testCmd.Output()
+// func Start(cmd string, args []string) string {
+// 	command := exec.Command(cmd, args...)
+// 	err := command.Start()
 // 	if err != nil {
-// 		panic(err)
-// 	}
-// 	fmt.Println(string(testOut))
 
-// 	return string(testOut)"
+// 		log.Fatal(err)
+// 	}
+// 	log.Printf("Waiting for command to finish...")
+// 	err = command.Wait()
+// 	log.Printf("Command finished with error: %v", err)
 // }
+
+// Run a command that is defined as an alias in ~/.bashrc or ~/.bash_aliases files.
+// The aliasCmd is the entire command you want to run.
+func RunAlias(aliasCmd string) string {
+	out, err := exec.Command("/bin/bash", "-c", aliasCmd).Output()
+	if err != nil {
+		logging.PrintlnError("error occured")
+		logging.PrintlnError(err)
+	}
+	logging.PrintlnVerbose(string(out))
+
+	return string(out)
+}
