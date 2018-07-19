cce357eb61528216c1665934e0b5389dbac2fa70
Unknown <remi.lavedrine@outlook.com>
2018-07-19 17:06:04 +0200

    Choice on which attack to perform. "[a]ll" or "[i]nsecure storage"

--- attacks/perform.go
+++ attacks/perform.go
@@ -5,15 +5,17 @@ import (
 	"github.com/shosta/androSecTest/terminal"
 )
 
+// Do :
 func Do(pkgname string) {
 	logging.Print(logging.Blue("What kind of attacks do you want to perform? [") +
-		logging.Red("A") +
+		logging.Red("a") +
 		logging.Blue("]ll, [") +
-		logging.Red("I") +
+		logging.Red("i") +
 		logging.Blue("]nsecure logging\n> "))
 	usrinput := terminal.Waitfor()
 
-	if usrinput == "i" {
+	if usrinput == "a" {
 		DoInsecureLog(pkgname)
+		DoInsecureStorage(pkgname)
 	}
 }
