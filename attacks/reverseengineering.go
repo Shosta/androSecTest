9098c91d96f7a0c97fcaa8b2bb6d95faab3da41f
Unknown <remi.lavedrine@outlook.com>
2018-07-20 11:48:44 +0200

    Add "reverse.go" to do reverse engineering on an apk
    It decodes an apk and store the source and res files to the "/decodedPackage" dir.
    It looks for some strings in the source and res files and store the result in the "/leakages" folder.

---
+++ attacks/reverseengineering.go
@@ -0,0 +1,44 @@
+package attacks
+
+import (
+	"github.com/shosta/androSecTest/command"
+	grep "github.com/shosta/androSecTest/command/grep"
+	"github.com/shosta/androSecTest/logging"
+	"github.com/shosta/androSecTest/variables"
+)
+
+// Voici le commande à utiliser :
+// ./ShostaSyncBox/Developpement/HackingTools/DecompilingAndroidApp/jadx/bin/jadx --deobf -d ~/android/security/com.orange.owtv/attacks/decodedPackage ~/android/security/com.orange.owtv/attacks/sourcePackage/com.orange.owtv.apk
+func reverseApk(apkname string) {
+
+	// TODO : Il faut changer le chemin absolu vers le binaire de jadx pour que cela soit rentré par l'utilisateur dans un fichier settings.
+	cmd := "~/ShostaSyncBox/Developpement/HackingTools/DecompilingAndroidApp/jadx/bin/jadx --deobf -d " +
+		"~/android/security/" + apkname + "/attacks/decodedPackage " +
+		"~/android/security/" + apkname + "/attacks/sourcePackage/" + apkname + ".apk"
+	logging.PrintlnDebug("Cmd : " + cmd)
+
+	logging.Println("Decompiling apk to " + logging.Bold(apkname+"/attacks/decodedPackage/"))
+	logging.Println("In progress...")
+	command.RunAlias(cmd)
+	logging.Println("Done")
+
+}
+
+// DoReverse :
+func DoReverse(pkgname string) {
+	logging.Println(logging.Green("Reverse apk"))
+	reverseApk(pkgname)
+	logging.Println(logging.Bold("Done"))
+
+	logging.Println(logging.Green("Check for leakage in codebase"))
+	checkForLeakage(pkgname)
+	logging.Println(logging.Bold("Done"))
+}
+
+func checkForLeakage(pkgname string) {
+	decoPkgPath := DecodedPackageDirPath(pkgname)
+	createLeakageDir(pkgname)
+	grep.Passwd(decoPkgPath, decoPkgPath+variables.LeakagesDir)
+	grep.Admin(decoPkgPath, decoPkgPath+variables.LeakagesDir)
+	grep.Key(decoPkgPath, decoPkgPath+variables.LeakagesDir)
+}
