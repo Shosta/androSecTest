297ae9a32ccea262135df2ea0c492358b16fea87
Unknown <remi.lavedrine@outlook.com>
2018-08-01 16:06:30 +0200

    Add a config.go file that does not have a build tag.

---
+++ config/config.go
@@ -0,0 +1,9 @@
+package config
+
+// Development ready configuration const based on the build tags.
+const (
+	SecurityAssessmentRootDir = "/home/shosta/android/security"
+	IsDebug                   = true
+)
+
+var IsVerboseLogRequired = true
